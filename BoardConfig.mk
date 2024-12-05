df5fe3c2626a3b9ca16d83b327b7ff5685629972
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-12-05 05:07:16 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-LH8n
+DEVICE_PATH := device/tecno/LH8n
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
