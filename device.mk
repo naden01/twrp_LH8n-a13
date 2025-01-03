22d9c5fe4cf452ec96577fb86ff0821309e6b5ee
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-01-03 07:22:16 -0800

    adding otacerts

--- device.mk
+++ device.mk
@@ -86,4 +86,8 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1 \
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1 \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1 
+	
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/releasekey
