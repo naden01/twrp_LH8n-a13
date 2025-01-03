a35d39ab78ff0991f62a618f6e2932476dd851e7
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-01-03 07:15:40 -0800

    fix vibration

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Patches
+RET=0
+cd bootable/recovery
+git apply ../../device/tecno/LH8n/patches/0001-Change-haptics-activation-file-path.patch > /dev/null 2>&1 || RET=$?
+cd ../../
+if [ $RET -ne 0 ];then
+    echo "ERROR: Patch is not applied! Maybe it's already patched?"
+else
+    echo "OK: All patched"
+fi
