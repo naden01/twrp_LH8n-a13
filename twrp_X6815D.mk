2f5c8c51c0ff724a07e3061ac7fcc3dda9a521a8
TegarXLu <botdimas67@gmail.com>
2025-04-15 09:10:32 +0700

    Update and rename twrp_LH8n.mk to twrp_X6815D.mk

--- twrp_X6815D.mk
+++ twrp_X6815D.mk
@@ -16,12 +16,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-LH8n device
-$(call inherit-product, device/tecno/LH8n/device.mk)
+$(call inherit-product, device/infinix/X6815D/device.mk)
 
-PRODUCT_DEVICE := LH8n
-PRODUCT_NAME := twrp_LH8n
-PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO LH8n
-PRODUCT_MANUFACTURER := tecno
+PRODUCT_DEVICE := X6815D
+PRODUCT_NAME := twrp_X6815D
+PRODUCT_BRAND := TegarXLu
+PRODUCT_MODEL := | INFINIX X6815D
+PRODUCT_MANUFACTURER := infinix
 
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
